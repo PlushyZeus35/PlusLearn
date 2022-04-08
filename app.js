1b901e31e0b3e00c9818fb7121701ef01c6238e9
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-04-08 19:42:14 +0200

    PLN-9: Updated tests & favicon

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const path = require('path')
+const favicon = require('serve-favicon');
 var indexRouter = require('./routes/index');
 
 const app = express();
 
 // Middlewares
 const myLogger = function (req, res, next) {
-    console.log('LOGGED')
-    next()
+    console.log('LOGGED');
+    next();
 }
 
+// Set default favicon
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'images', 'brand', 'favicon.ico')));
+
 // Set static path to serve static files
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // Set Pug Template Engine
 // Set directory where the template files are located
-app.set('views', './views')
+app.set('views', './views');
 // Set template engine to use
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 // Set middlewares
 // app.use(myLogger);
