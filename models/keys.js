281d63615fc87e72deef66a38f3eceb6fd780c57
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-05-21 13:09:14 +0200

    Fixed database connection

--- models/keys.js
+++ models/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     database: {
-        host: '192.168.0.200',
+        host: 'pluslearn.ddns.net',
         user: 'plushlearn',
         password: 'plushlearnmariadb',
         database: 'plusLearnDev',
