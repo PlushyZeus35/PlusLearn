6cd4d62affa5ff9a634fa988cfc7d1c1c4071e22
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-05-27 13:55:43 +0200

    PLN-25: Updated user signup validations

--- models/User.js
+++ models/User.js
@@ -21,10 +21,7 @@ User.init({
     },
     email: {
         type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
+        allowNull: false
     },
     role: DataTypes.INTEGER
 }, {
