49fd0153f9afc9121d8aa73a33713c8e4752fe3b
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-05-27 13:56:28 +0200

    PLN-25: fixed database connection

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         password: "plushlearnmariadb",
         database: "plusLearnDev_dev",
-        host: "192.168.0.200",  //pluslearn.ddns.net o 192.168.0.200 si estoy en local
+        host: "pluslearn.ddns.net",  //pluslearn.ddns.net o 192.168.0.200 si estoy en local
         user: 'plushlearn',
         port: 3306
     }
