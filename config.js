484e87a55300012fb9c02d70921ce2ec47d10172
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-07-03 01:22:58 +0200

    PLN-26: Set DB conf

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        password: "975ae948",
-        database: "heroku_ef7baac88d3e2e0",
-        host: "eu-cdbr-west-02.cleardb.net",  //pluslearn.ddns.net o 192.168.0.200 si estoy en local
-        user: 'ba79a05ef88816',
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE_DEV,
+        host: process.env.DATABASE_HOST,  //pluslearn.ddns.net o 192.168.0.200 si estoy en local
+        user: process.env.CLEARDB_DATABASE_USERNAME,
         port: 3306
     }
 }
