9a343db3872a5d3be6adb5b9999b2cfe22a69b19
Borja Lorenzo Adajas <borjalorenzoadajas@usal.es>
2022-07-03 01:17:15 +0200

    PLN-26: Improve frontend

--- public/js/editquiz.js
+++ public/js/editquiz.js
@@ -1,3 +1,35 @@
+
+function cronstructChart(){
+    const lineChartlabels = ['Enero','Febrero','Marzo','Abril','Mayo','Jumio','Julio'];
+    const lineChartData = {
+        labels: lineChartlabels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        },{
+            label: 'My Second Dataset',
+            data: [20, 34, 74, 85, 48, 64, 12],
+            fill: false,
+            borderColor: 'rgb(128, 212, 255)',
+            tension: 0.1
+        },{
+            label: 'My Third Dataset',
+            data: [50,50,50,50,50,50,50],
+            fill: false,
+            borderColor: 'rgb(153, 0, 102)',
+            tension: 0.1
+        }]
+    };
+    const lineChartConfig = {
+        type: 'line',
+        data: lineChartData,
+    };
+    const chart2 = document.getElementById('myChart2').getContext('2d');
+    const myChart2 = new Chart(chart2, lineChartConfig);
+}
 function editQuestion(id){
     console.log(id);
     var questionNameId = "#questionName" + id;
@@ -5,7 +37,7 @@ function editQuestion(id){
     var questionIncorrect1Id = "#incorrectResponse1" + id;
     var questionIncorrect2Id = "#incorrectResponse2" + id;
     var questionIncorrect3Id = "#incorrectResponse3" + id;
-    console.log
+    
     var questionNameField = $(questionNameId)[0];
     var correctResponseField = $(questionCorrectId)[0];
     var incorrectResponse1Field = $(questionIncorrect1Id)[0];
